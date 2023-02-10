8bcc5f621eee340a247bc5e7f57cf86997e99227
Juan Andrés <juan.hurtado10@usc.edu.co>
2023-02-10 16:07:38 -0500

    Juan Andrés

---
+++ Principal/src/Principal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//nombre de la clase
+public class Principal {
+
+	// clase que permite capturar datos por consola
+	Scanner sc = new Scanner(System.in);
+
+	// metodos principal
+	public static void main(String[] args) {
+
+		System.out.println("ingrese la palabra");
+
+		try (// clase que permite capturar datos por consola
+				Scanner sc = new Scanner(System.in)) {
+			String prueba = sc.next();
+			System.out.println("la cantidad de letras que tiene la palabra es : " + prueba.toUpperCase());
+		}
+
+	}
+}
