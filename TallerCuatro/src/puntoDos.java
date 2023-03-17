f49e89a8d5f4d4eb7757007b7cc77f490fa38c2d
Juan Andrés <125085447+juanxvss@users.noreply.github.com>
2023-03-17 11:56:23 -0500

    Update puntoDos.java

--- TallerCuatro/src/puntoDos.java
+++ TallerCuatro/src/puntoDos.java
@@ -1,4 +1,45 @@
+import java.util.Scanner;
 
 public class puntoDos {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce el primer número: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Introduce el segundo número: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Introduce el operador (+, -, *, / o %): ");
+        String operator = scanner.next();
+        
+        double result;
+        switch(operator) {
+            case "+":
+                result = num1 + num2;
+                System.out.println("El resultado de la suma es: " + result);
+                break;
+            case "-":
+                result = num1 - num2;
+                System.out.println("El resultado de la resta es: " + result);
+                break;
+            case "*":
+                result = num1 * num2;
+                System.out.println("El resultado de la multiplicación es: " + result);
+                break;
+            case "/":
+                result = num1 / num2;
+                System.out.println("El resultado de la división es: " + result);
+                break;
+            case "%":
+                result = num1 % num2;
+                System.out.println("El resultado del módulo es: " + result);
+                break;
+            default:
+                System.out.println("El operador introducido no es válido.");
+                break;
+        }
+        
+        scanner.close();
+    }
 
 }
