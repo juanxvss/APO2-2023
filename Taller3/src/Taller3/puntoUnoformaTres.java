c81e202a71a8403e1af3eb6ab7bc33b7fd0fc262
Juan Andrés <125085447+juanxvss@users.noreply.github.com>
2023-03-17 11:12:24 -0500

    Update puntoUnoformaTres.java

--- Taller3/src/Taller3/puntoUnoformaTres.java
+++ Taller3/src/Taller3/puntoUnoformaTres.java
@@ -1,5 +1,8 @@
 package Taller3;
 
 public class puntoUnoformaTres {
-
+    public static void main(String[] args) {
+        char[] charArray = {'a', 'b', 'c'};
+        String myString = new String(charArray);
+    }
 }
