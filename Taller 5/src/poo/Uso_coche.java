946c02f7d3c2c486326915ca20a1a543b8ff6910
Juan Andrés <125085447+juanxvss@users.noreply.github.com>
2023-03-17 14:47:04 -0500

    Add files via upload

---
+++ Taller 5/src/poo/Uso_coche.java
@@ -0,0 +1,28 @@
+package poo;
+
+public class Uso_coche {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Coche micoche = new Coche();
+
+        micoche.establece_color("amarillo");
+
+        System.out.println(micoche.dime_asientos1());
+
+        System.out.println(micoche.dime_color());
+
+        micoche.configura_asientos("si");
+
+        System.out.println(micoche.dime_asientos());
+
+        micoche.configura_climatizador("si");
+
+        System.out.println(micoche.dime_climatizador());
+
+        System.out.println(micoche.dime_peso_coche());
+
+        System.out.println("El precio final del coche es: " + micoche.precio_coche() + " euros.");
+    }
+}
