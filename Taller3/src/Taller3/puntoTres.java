64f6b9d97e1c841e5e6c146556e3f5fd437f56ed
Juan Andrés <125085447+juanxvss@users.noreply.github.com>
2023-03-17 11:11:06 -0500

    Update puntoTres.java

--- Taller3/src/Taller3/puntoTres.java
+++ Taller3/src/Taller3/puntoTres.java
@@ -1,5 +1,10 @@
 package Taller3;
 
 public class puntoTres {
-
+  public static void main(String[] args) {
+    String str1 = "Hola";
+    String str2 = "mundo";
+    String str3 = str1 + " " + str2;
+    System.out.println(str3);
+  }
 }
